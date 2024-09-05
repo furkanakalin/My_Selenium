11ad4bfcb596094734350450be16c9cff550ca8a
furkanakalin <furkanakalin3423@gmail.com>
2024-09-05 13:21:27 +0300

    Gun07-Soru01 tamamlandı

--- src/Gun07/Soru01.java
+++ src/Gun07/Soru01.java
@@ -2,9 +2,12 @@ package Gun07;
 
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Soru01 extends BaseDriver {
 //        Senaryo
@@ -18,15 +21,14 @@ public class Soru01 extends BaseDriver {
 //        8- kullanıcı bilgilerini doldurup Continue ya tıklatın
 //        9- Burada her bir eşyanın ücretlerinin toplamının aşağıdaki
 //        Item total e eşit olup olmadığını Assert ile test ediniz.
-//
 //        Bu soruda tamamen XPATH kullanılacaktır.
-    //    Assert.assertTrue(urunFiyatToplamlam == itemTotal)
-
+//        Assert.assertTrue(urunFiyatToplamlam == itemTotal)
 
     @Test
     public void Test(){
 
         driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
 
         WebElement username = driver.findElement(By.xpath("//*[@placeholder='Username']"));
         username.sendKeys("standard_user");
@@ -48,6 +50,7 @@ public class Soru01 extends BaseDriver {
         addcard1.click();
         MyFunc.Bekle(2);
         driver.navigate().back();
+        MyFunc.Bekle(2);
 
         WebElement item2 = driver.findElement(By.xpath("//*[text()='Sauce Labs Bolt T-Shirt']"));
         item2.click();
@@ -79,7 +82,26 @@ public class Soru01 extends BaseDriver {
 
         WebElement continuee = driver.findElement(By.xpath("//*[@id='continue']"));
         continuee.click();
-        MyFunc.Bekle(2);
+
+        List<WebElement> itemprices =driver.findElements(By.xpath("//*[@class='inventory_item_price']"));
+        Double totalprice=0.0;
+        for (WebElement itemprice: itemprices){
+            String item=itemprice.getText().replace("$","");
+            Double doubleitem=Double.parseDouble(item);
+            totalprice=doubleitem+totalprice;
+        }
+        String strtotalprice =Double.toString(totalprice);
+
+        WebElement itemtotal = driver.findElement(By.xpath("//*[@class='summary_subtotal_label']"));
+        String totalitem = itemtotal.getText();
+
+        Assert.assertTrue("Sepet toplamını tekrar kontrol et", totalitem.contains(strtotalprice));
+
+        System.out.println("Test passed");
+
+        BekleKapat();
+
+
 
 
 
